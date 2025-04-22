b004c76f910d0161ef18a125bfe32771e9887117
unknown <loyoenock@gmail.com>
2025-04-22 17:40:13 +0300

    Component - Navigation completed

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,20 @@ import './App.css'
 import "./index.css"
 import { LoadingScreen } from './components/LoadingScreen'
 import { useState } from 'react'
+import { Navbar } from './components/Navbar'
+import { MobileNav } from './components/sections/MobileNav'
 
 function App() {
   const [isLoading, setIsLoading] = useState(false)
+  const [menuOpen, setMenuOpen] = useState(false);
 
   return (
     <>
-      {!isLoading && <LoadingScreen onComplete={() => setIsLoading(true)} />}
+      {!isLoading && <LoadingScreen onComplete={() => setIsLoading(true)} />}{" "}
+        <div className={`min-h-screen transition-opacity duration-700 ${isLoading ? 'opacity' : 'opacity-0'} bg-black text-gray-100`}>
+          <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+          <MobileNav menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+        </div>
     </>
   )
 }
